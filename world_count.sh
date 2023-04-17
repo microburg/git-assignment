5e0cadcb585747dfbaa4b2748d5bda0c7b10204e
microburg <s-omar.hamed@zewailcity.edu.eg>
2023-04-17 21:16:30 +0200

    changed the bash file

--- world_count.sh
+++ world_count.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+if [$# -eq 0]; then
+	echo "usage: $0 directory_path"
+	exit 1
+fi
+
+if [! -d"$1"]; then
+	echo "invlaid directory"
+	exit 1
+fi
+
+
+
+
 words=0
-for file in *.txt; do
+for file in "$1"/*.txt; do
 	N=$(cat "$file"|wc -w)
 words=$((words+N))
 done
 
-echo "The number of words in this repo is: $words"
+echo "The number of words in repo $1: $words"
 
